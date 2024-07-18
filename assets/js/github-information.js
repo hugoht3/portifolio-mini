a7f711c6fed322a6133d27f46ae10cc9a4a60cc5
Victor Hugo Barili Schons <hugoht3@hotmail.com>
2024-07-18 16:55:22 +0000

    fixed bug whit empety divs

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,30 +1,28 @@
 function userInformationHTML(user) {
-    return `<h2>${user.name}
-        <span class="small-name">
-            (@<a href="${user.html_url}" target="_blank>${user.login}</a>)
-        </span>
-    </h2>
-    <div class="gh-content">
-        <div class="gh-avatar>
-            <a href="${user.html_url} target="_blank>
-                <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
-            </a>
-        </div>
-        <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
-    
-    </div>`;
-
-
+    return `
+        <h2>${user.name}
+            <span class="small-name">
+                (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
+            </span>
+        </h2>
+        <div class="gh-content">
+            <div class="gh-avatar">
+                <a href="${user.html_url}" target="_blank">
+                    <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
+                </a>
+            </div>
+            <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
+        </div>`;
 }
 
 function repoInformationHTML(repos) {
-    if (repos.lenght == 0) {
+    if (repos.length == 0) {
         return `<div class="clearfix repo-list">No repos!</div>`;
     }
 
     var listItemsHTML = repos.map(function(repo) {
         return `<li>
-                    <a href ="${repo.html_url}" target="_blank">${repo.name}</a>
+                    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
                 </li>`;
     });
 
@@ -35,15 +33,16 @@ function repoInformationHTML(repos) {
                 <ul>
                     ${listItemsHTML.join("\n")}
                 </ul>
-            </div>`
+            </div>`;
 }
 
-
 function fetchGitHubInformation(event) {
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
 
     var username = $("#gh-username").val();
     if (!username) {
-        $("#gh-user-data").html(`<h2><strong>Please enter a GitHub username</strong></h2>`);
+        $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
         return;
     }
 
@@ -62,14 +61,16 @@ function fetchGitHubInformation(event) {
             $("#gh-user-data").html(userInformationHTML(userData));
             $("#gh-repo-data").html(repoInformationHTML(repoData));
         },
-        function (errorResponse) {
+        function(errorResponse) {
             if (errorResponse.status === 404) {
-                $("#gh-user-data").html(`<h2>No info found for user ${username}</h2>`);
+                $("#gh-user-data").html(
+                    `<h2>No info found for user ${username}</h2>`);
             } else {
                 console.log(errorResponse);
-                $("#hg-user-data").html(
+                $("#gh-user-data").html(
                     `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
-
             }
         });
 }
+
+$(document).ready(fetchGitHubInformation);
